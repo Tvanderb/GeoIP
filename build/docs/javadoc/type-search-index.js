6876e37040947322b8a989bf1661822e7afca152
Talon Vanderbeken <xxtvanderb@gmail.com>
2020-10-27 13:04:43 -0400

    Gradle clean

--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"com.github.tvanderb.geoip.api","l":"AddressInfo"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.github.tvanderb.geoip.api","l":"Location"},{"p":"com.github.tvanderb.geoip","l":"Test"}]
--- src/main/java/com/github/tvanderb/geoip/Test.java
+++ src/main/java/com/github/tvanderb/geoip/Test.java
@@ -9,7 +9,9 @@ public class Test {
     public static void main(String... args) throws IOException {
         System.out.println("Location: " + Location.getMyLocation());
         System.out.println("Info: " + AddressInfo.getMyAddress());
+
         System.out.println("\n--------------------------\n");
+
         System.out.println("Location: " + Location.getLocation("google.com"));
         System.out.println("Info: " + AddressInfo.getInfo("google.com"));
     }
